bbeecf463294b6c59324d07097858f173bd9ba03
KaidoSo <kaido.soomlais@gmail.com>
2021-05-03 13:36:58 +0300

    added questions and pictures

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log('hey there');
+// Questions object
+var questions = [{
+    question: "What does Michael pretend to fire Pam over in season one?",
+    imgSrc: "img/question-1.webp",
+    answer1: "Stealing post-it notes",
+    answer2: "Not answering the phone",
+    answer3: "Wearing her glasses",
+    correct: "answer1"
+},
+{
+    question: "What does Kevin suggest Dwight put in his gun holster?",
+    imgSrc: "img/question-2.webp",
+    answer1: "A cell phone",
+    answer2: "A banana",
+    answer3: "A toy gun",
+    correct: "answer2"
+},
+{
+    question: "How much did Michael's beloved plasma TV cost?",
+    imgSrc: "img/question-3.webp",
+    answer1: "100$",
+    answer2: "500$",
+    answer3: "200$",
+    correct: "answer3"
+},
+{
+    question: "What kind of sauce does Kevin ask for when being forced to eat broccoli?",
+    imgSrc: "img/question-4.webp",
+    answer1: "Kethcup",
+    answer2: "Hollandaise",
+    answer3: "Mayonaise",
+    correct: "answer2"
+},
+{
+    question: "When role-playing a successful sales call with Jim and Michael, who does Dwight offend on the phone?",
+    imgSrc: "img/question-5.webp",
+    answer1: "Fudd G. Packer",
+    answer2: "Moe Lester",
+    answer3: "William M. Buttlicker",
+    correct: "answer3"
+}
+]
+
